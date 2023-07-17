84a5c052361fed261dacf6978b24deccdbd35b3e
‘muddyrain’ <qiu3291002845@gmail.com>
2023-07-17 22:40:32 +0800

    feat: 备份

--- Tank-Game/Form1.cs
+++ Tank-Game/Form1.cs
@@ -15,6 +15,34 @@ namespace Tank_Game
         public Form1()
         {
             InitializeComponent();
+
+            // 开始位置居中
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+
+            #region 画线
+            //g.DrawLine(new Pen(Color.Red, 2), new Point(0, 0), new Point(100, 100));
+            #endregion
+
+            //g.DrawString(
+            //    "Muddyrain",
+            //    new Font("微软雅黑", 20),
+            //    new SolidBrush(Color.Red),
+            //    new Point(100, 100)
+            //    );
+            Image image = Properties.Resources.Boss;
+
+            Bitmap bm = Properties.Resources.Star1;
+            bm.MakeTransparent(Color.Black);
+            g.DrawImage(image, 0, 0, 100, 100);
+            g.DrawImage(bm, 100, 100, 100, 100);
         }
     }
 }
